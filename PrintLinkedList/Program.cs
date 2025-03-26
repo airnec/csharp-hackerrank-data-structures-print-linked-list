dc7e4eaa50057ed511465d9efe38a4865487fa53
Necip Dirik <necipdirik@gmail.com>
2025-03-26 22:06:44 +0300

    Add project files.

---
+++ PrintLinkedList/Program.cs
@@ -0,0 +1,40 @@
+﻿using PrintLinkedList;
+
+SinglyLinkedListNode head = new SinglyLinkedListNode(10);
+SinglyLinkedListNode node2 = new SinglyLinkedListNode(20);
+SinglyLinkedListNode node3 = new SinglyLinkedListNode(30);
+
+head.Next = node2;
+head.Next.Next = node3;
+
+PrintLinkedList.Result.PrintLinkedList(head);
+
+namespace PrintLinkedList
+{
+    public class SinglyLinkedListNode
+    {
+        public int Data { get; set; }
+        public SinglyLinkedListNode? Next { get; set; }
+
+        public SinglyLinkedListNode(int nodeData)
+        {
+            this.Data = nodeData;
+            this.Next = null;
+        }
+    }
+
+    public static class Result
+    {
+        public static void PrintLinkedList(SinglyLinkedListNode head)
+        {
+            SinglyLinkedListNode? current = head;
+
+            while (current != null)
+            {
+                Console.WriteLine(current.Data);
+                current = current.Next;
+            }
+        }
+    }
+}
+
